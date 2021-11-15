f361b965eca80942fd4f88f754b7dd60b44877a4
SAHIL-GARG-L\sahil.garg <sahil.garg@quovantis.com>
2021-11-15 10:19:43 +0530

    removed react files

--- utils/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-import { colorPrimary } from "./colors";
-
-export const globalStyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: colorPrimary
-    }
-});
